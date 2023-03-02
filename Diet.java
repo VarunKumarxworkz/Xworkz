8f4a1a59b2f4042a2dbb5bf55229aa70cfa7a3b4
varunkumarxworkz <varunkumarxworz@gmail.com>
2023-03-02 21:34:39 +0530

    First-code

---
+++ Diet.java
@@ -0,0 +1,13 @@
+public class Diet{
+	public static void main(String[] args) {
+    Workout();
+
+	}
+	public static void Workout(){
+	
+	  System.out.println("Bicep Size is 15 inch");
+	  
+	
+	}
+	
+}
