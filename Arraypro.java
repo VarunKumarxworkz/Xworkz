563001d5495ad182168ab73769b14a2d9e5630d2
varunkumarxworkz <varunkumarxworz@gmail.com>
2023-03-04 22:33:03 +0530

    array-code

---
+++ Arraypro.java
@@ -0,0 +1,26 @@
+public class Arraypro{
+    public static void main(String[] args) {
+      int[] Height = {120,130,140,150,160,170,175,180,185,190};
+	  String[] Name = {"HrithikRoshan","Rock","Ronnie","CBum","jeremyBuendia","Ullises","Kaigreen","RajaAjith","Potvin","Jose"};
+	   byte[] GymMembers = {50,55,60,65,70,75,80,85,90,95};
+       short[] PriceOfPowders = {500,700,1000,1400,1800,2000,2559,2999,3800,5000};
+       long[] PriceOfCars = {500000,600000,700000,800000,900000,850000,750000,650000,550000,950000};
+	   float[] Bmi = {15.75f,15.92f,16.91f,17.97f,18.92f,2.63f,2.55f,2.48f,2.72f,2.83f};
+	   double[] Weight = {50.200d,55.700d,60.115d,65.105d,70.320d,75.900d,80.550d,85.560d,90.765d,95.885d};
+	   char[] CompatitorsSets = {'A','B','C','D','E','F','G','H','I','J'};
+	   boolean[] Placed = {true,false,true,true,false,true,false,false,true,false};
+	   
+
+
+       System.out.println(GymMembers[5]);
+       System.out.println(PriceOfPowders[6]);
+       System.out.println(PriceOfCars[7]);
+       System.out.println(Bmi[4]);
+       System.out.println(Weight[3]);
+       System.out.println(CompatitorsSets[7]);
+       System.out.println(Placed[8]);
+	   System.out.println(Height[7]);
+	   System.out.println(Name[5]);
+	
+	}
+}
