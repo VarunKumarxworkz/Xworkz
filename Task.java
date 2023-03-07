4ea1dc4f877e65b11983089f9b45a2408e745f2e
varunkumarxworkz <varunkumarxworz@gmail.com>
2023-03-07 23:23:58 +0530

    task

---
+++ Task.java
@@ -0,0 +1,26 @@
+ public class Task{
+ 
+      public static void main (String[] args) {
+	      int number = 101;
+		  FirstMethod();
+	  
+	  }
+       
+	   public static void FirstMethod() {
+	       String language = "English";
+		   SecondMethod();
+	   
+	   }
+        public static void SecondMethod() {
+		    byte Mark = 72;
+			ThirdMethod();
+		
+		}
+        public static void ThirdMethod() {
+		      System.out.println("Executing third method");
+		}
+ 
+ 
+ 
+ 
+ }
