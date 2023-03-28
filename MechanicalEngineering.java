7681947e79bd9eec6265e543c2e7384f949adacb
varunkumarxworkz <varunkumarxworz@gmail.com>
2023-03-28 09:54:16 +0530

    state and behaviour

---
+++ MechanicalEngineering.java
@@ -0,0 +1,530 @@
+public class MechanicalEngineering{
+
+static short Coursecode;
+String graduation;
+String PRINCIPAL;
+String College;
+short collegecode;
+int No_of_sem;
+int no_of_dept;
+short no_of_staff;
+int messfee;
+int accomedation;
+int academicfee;
+int tutionfee;
+boolean is_scopeinfuture;
+short hodexperience;
+short otherstaffexperience;
+boolean practicalknowledge;
+boolean practicalequipments;
+boolean booksavailable;
+int number_of_mainpaper;
+int no_of_practicalpaper;
+ 
+boolean isvalueaddedcourse_available;
+String coursename;
+String domain;
+int durationofcourse;
+String modeofcourse;
+int fees;
+int no_of_periods;
+boolean islapneed;
+boolean insatllment;
+float pointingforcourse;
+boolean isituseful;
+int numof_assessment;
+String potofolio;
+String capstoneproject;
+boolean isnecessaryforcasestudy;
+int tasks;
+boolean havediscussionprompt;
+String assignmentsbelike;
+boolean isitunderstandable;
+boolean isitprovideceritificate;
+
+int first_sem;
+int first_no_of_sub;
+String first_hod;
+String first_advisor;
+String first_tutor;
+String first_mentor;
+int first_no_of_mentors;
+int first_no_of_easysub;
+int first_no_of_hardsub;
+int first_cycletest;
+int first_internals;
+int first_praticles;
+int first_easysub;
+int first_hardsub;
+int first_studentlist;
+int first_assignment;
+int first_model;
+int first_miniproject;
+int first_schedule;
+int first_rep;
+
+int sec_sem;
+int sec_no_of_sub;
+String sec_hod;
+String sec_advisor;
+String sec_tutor;
+String sec_mentor;
+int sec_no_of_mentors;
+int sec_no_of_easysub;
+int sec_no_of_hardsub;
+int sec_cycletest;
+int sec_internals;
+int sec_praticles;
+int sec_easysub;
+int sec_hardsub;
+int sec_studentlist;
+int sec_assignment;
+int sec_model;
+int sec_miniproject;
+int sec_schedule;
+int sec_rep;
+
+int third_sem;
+int third_no_of_sub;
+String third_hod;
+String third_advisor;
+String third_tutor;
+String third_mentor;
+int third_no_of_mentors;
+int third_no_of_easysub;
+int third_no_of_hardsub;
+int third_cycletest;
+int third_internals;
+int third_praticles;
+int third_easysub;
+int third_hardsub;
+int third_studentlist;
+int third_assignment;
+int third_model;
+int third_miniproject;
+int third_schedule;
+int third_rep;
+
+int IVTH_rep_sem;
+int IV_no_of_sub;
+String IV_hod;
+String IV_advisor;
+String IV_tutor;
+String IV_mentor;
+int IV_no_of_mentors;
+int IV_no_of_easysub;
+int IV_no_of_hardsub;
+int IV_cycletest;
+int IV_internals;
+int IV_praticles;
+int IV_easysub;
+int IV_hardsub;
+int IV_studentlist;
+int IV_assignment;
+int IV_model;
+int IV_miniproject;
+int IV_schedule;
+int IVTH_rep;
+
+int VTH_rep_sem;
+int V_no_of_sub;
+String V_hod;
+String V_advisor;
+String V_tutor;
+String V_mentor;
+int V_no_of_mentors;
+int V_no_of_easysub;
+int V_no_of_hardsub;
+int V_cycletest;
+int V_internals;
+int V_praticles;
+int V_easysub;
+int V_hardsub;
+int V_studentlist;
+int V_assignment;
+int V_model;
+int V_miniproject;
+int V_schedule;
+int VTH_rep;
+
+int VITH_rep_sem;
+int VI_no_of_sub;
+String VI_hod;
+String VI_advisor;
+String VI_tutor;
+String VI_mentor;
+int VI_no_of_mentors;
+int VI_no_of_easysub;
+int VI_no_of_hardsub;
+int VI_cycletest;
+int VI_internals;
+int VI_praticles;
+int VI_easysub;
+int VI_hardsub;
+int VI_studentlist;
+int VI_assignment;
+int VI_model;
+int VI_miniproject;
+int VI_schedule;
+int VITH_rep;
+
+int VIITH_rep_sem;
+int VII_no_of_sub;
+String VII_hod;
+String VII_advisor;
+String VII_tutor;
+String VII_mentor;
+int VII_no_of_mentors;
+int VII_no_of_easysub;
+int VII_no_of_hardsub;
+int VII_cycletest;
+int VII_internals;
+int VII_praticles;
+int VII_easysub;
+int VII_hardsub;
+int VII_studentlist;
+int VII_assignment;
+int VII_model;
+int VII_miniproject;
+int VII_schedule;
+int VIITH_rep;
+
+int VIIITH_rep_sem;
+int VIII_no_of_sub;
+String VIII_hod;
+String VIII_advisor;
+String VIII_tutor;
+String VIII_mentor;
+int VIII_no_of_mentors;
+int VIII_no_of_easysub;
+int VIII_no_of_hardsub;
+int VIII_cycletest;
+int VIII_internals;
+int VIII_praticles;
+int VIII_easysub;
+int VIII_hardsub;
+int VIII_studentlist;
+int VIII_assignment;
+int VIII_model;
+int VIII_miniproject;
+int VIII_schedule;
+int VIIITH_rep;
+
+public static void main(String[] args){
+	
+}
+public void SEMItake_lessons(){
+	
+}
+public void SEMcompletingLESSON(){
+	
+}
+public void SEMI_testwriting(){
+	
+	
+}
+public void  SEMI_paperdistribution(){
+	
+}
+public void  SEMIlisten(){
+	
+}
+public void SEMIWRING_ESSAY(){
+}
+public void SEMI_ask(){
+	
+}
+public void SEMI_completeassignment(){
+	
+}
+public void SEMI_submitasignment(){
+	
+}
+public void  SEMI_pptpresentation(){
+	
+}
+public void  SEMIattendclass(){
+	
+}
+public void SEMIattendence(){
+	
+}
+public void SEMIobeystaffs(){
+	
+}
+public void SEMIfollowrules(){
+	
+}
+public void SEMImakemodel(){
+	
+}
+
+
+	public void SEMIItake_lessons(){
+	
+}
+public void SEMIIcompletingLESSON(){
+	
+}
+public void SEMII_testwriting(){
+	
+	
+}
+public void  SEMII_paperdistribution(){
+	
+}
+public void  SEMIIlisten(){
+	
+}
+public void SEMIIWRING_ESSAY(){
+}
+public void SEMII_ask(){
+	
+}
+public void SEMII_completeassignment(){
+	
+}
+public void SEMII_submitasignment(){
+	
+}
+public void  SEMII_pptpresentation(){
+	
+}
+public void  SEMIIattendclass(){
+	
+}
+public void SEMIIattendence(){
+	
+}
+public void SEMIIobeystaffs(){
+	
+}
+public void SEMIIfollowrules(){
+	
+}
+public void SEMIImakemodel(){
+	
+}
+
+
+	public void SEMIIItake_lessons(){
+	
+}
+public void SEMIIIcompletingLESSON(){
+	
+}
+public void SEMIII_testwriting(){
+	
+	
+}
+public void  SEMIII_paperdistribution(){
+	
+}
+public void  SEMIIIlisten(){
+	
+}
+public void SEMIIIWRING_ESSAY(){
+}
+public void SEMIII_ask(){
+	
+}
+public void SEMIII_completeassignment(){
+	
+}
+public void SEMIII_submitasignment(){
+	
+}
+public void  SEMIII_pptpresentation(){
+	
+}
+public void  SEMIIIattendclass(){
+	
+}
+public void SEMIIIattendence(){
+	
+}
+public void SEMIIIobeystaffs(){
+	
+}
+public void SEMIIIfollowrules(){
+	
+}
+public void SEMIIImakemodel(){
+	
+}
+
+
+public void SEMIVtake_lessons(){
+	
+}
+public void SEMIVcompletingLESSON(){
+	
+}
+public void SEMIV_testwriting(){
+	
+	
+}
+public void  SEMIV_paperdistribution(){
+	
+}
+public void  SEMIVlisten(){
+	
+}
+public void SEMIVWRING_ESSAY(){
+}
+public void SEMIV_ask(){
+	
+}
+public void SEMIV_completeassignment(){
+	
+}
+public void SEMIV_submitasignment(){
+	
+}
+public void  SEMIV_pptpresentation(){
+	
+}
+public void  SEMIVattendclass(){
+	
+}
+public void SEMIVattendence(){
+	
+}
+public void SEMIVobeystaffs(){
+	
+}
+public void SEMIVfollowrules(){
+	
+}
+public void SEMIVmakemodel(){
+	
+}
+
+
+public void SEMVtake_lessons(){
+	
+}
+public void SEMVcompletingLESSON(){
+	
+}
+public void SEMV_testwriting(){
+	
+	
+}
+public void  SEMV_paperdistribution(){
+	
+}
+public void  SEMVlisten(){
+	
+}
+public void SEMVWRING_ESSAY(){
+}
+public void SEMV_ask(){
+	
+}
+public void SEMV_completeassignment(){
+	
+}
+public void SEMV_submitasignment(){
+	
+}
+public void  SEMV_pptpresentation(){
+	
+}
+public void  SEMVattendclass(){
+	
+}
+public void SEMVattendence(){
+	
+}
+public void SEMVobeystaffs(){
+	
+}
+public void SEMVfollowrules(){
+	
+}
+public void SEMVmakemodel(){
+	
+}
+
+	public void SEMVItake_lessons(){
+	
+}
+public void SEMVIcompletingLESSON(){
+	
+}
+public void SEMVI_testwriting(){
+	
+	
+}
+public void  SEMVI_paperdistribution(){
+	
+}
+public void  SEMIVIlisten(){
+	
+}
+public void SEMVIWRING_ESSAY(){
+}
+public void SEMVI_ask(){
+	
+}
+public void SEMVI_completeassignment(){
+	
+}
+public void SEMVI_submitasignment(){
+	
+}
+public void  SEMVI_pptpresentation(){
+	
+}
+public void  SEMVIattendclass(){
+	
+}
+public void SEMVIattendence(){
+	
+}
+public void SEMVIobeystaffs(){
+	
+}
+public void SEMVIfollowrules(){
+	
+}
+public void SEMVImakemodel(){
+	
+}
+
+
+public void SEMVIItake_lessons(){
+	
+}
+public void SEMVIIcompletingLESSON(){
+	
+}
+public void SEMVII_testwriting(){
+	
+	
+}
+public void  SEMVII_paperdistribution(){
+	
+}
+public void  SEMVIIlisten(){
+	
+}
+public void SEMVIIWRING_ESSAY(){
+}
+public void SEMVII_ask(){
+	
+}
+public void SEMVII_completeassignment(){
+	
+}
+public void SEMVII_submitasignment(){
+	
+}
+public void  SEMVII_pptpresentation(){
+	
+}
+ 
+
+
+}
